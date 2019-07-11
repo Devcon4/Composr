8aefd256cbc7b00dc2a6c0438d4fbd0db6046942
Gerald Murphy <gerald.murphy@centeva.com>
2019-07-11 13:06:51 -0600

    code cleanup

--- api/src/controller/evaluateController.ts
+++ api/src/controller/evaluateController.ts
@@ -1,11 +1,11 @@
 import { ioc } from "../ioc";
 import { IController } from "./iController";
+import { Response } from "node-fetch";
 
 export class EvaluateController implements IController {
     constructor() {
         ioc.router.get('/evaluate', ctx => {
             ctx.body = 'Test eval!';
-            ctx.response = {'test': 'testVluae'}
         });
     }
 }
