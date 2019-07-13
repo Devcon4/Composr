f5d5564d11aede88f539ce9687e1861601b14e26
devyn cyphers <devyn.cyphers@centeva.com>
2019-07-12 22:39:49 -0600

    merge

--- client/src/app/component/uploader/uploader.component.ts
+++ client/src/app/component/uploader/uploader.component.ts
@@ -33,16 +33,10 @@ export class UploaderComponent implements OnInit {
       const reader = new FileReader();
 
       reader.readAsDataURL(event.target.files[0]); // read file as data url
-<<<<<<< HEAD
       reader.onload = async (fileReader: any) => { // called once readAsDataURL is completed
         this.primaryPhoto = fileReader.target.result;
         this.newPrimaryPhoto.emit(this.primaryPhoto);
         this.uploadCall(this.patternPhoto);
-=======
-      reader.onload = (fileReader: any) => { // called once readAsDataURL is completed
-        this.primaryPhoto = fileReader.target.result;
-        this.newPrimaryPhoto.emit(this.primaryPhoto);
->>>>>>> 99dc85e4248fc68005e6b603b3cdb843463a4ad6
       };
     }
   }
@@ -52,28 +46,18 @@ export class UploaderComponent implements OnInit {
       const reader = new FileReader();
 
       reader.readAsDataURL(event.target.files[0]); // read file as data url
-<<<<<<< HEAD
       reader.onload = (fileReader: any) => { // called once readAsDataURL is completed
         this.patternPhoto = fileReader.target.result;
         this.newPatternPhoto.emit(this.patternPhoto);
         this.uploadCall(this.patternPhoto);
-=======
-
-      reader.onload = (fileReader: any) => { // called once readAsDataURL is completed
-        this.patternPhoto = fileReader.target.result;
-        this.newPatternPhoto.emit(this.patternPhoto);
->>>>>>> 99dc85e4248fc68005e6b603b3cdb843463a4ad6
       };
     }
   }
 
-<<<<<<< HEAD
   uploadCall(image: string) {
     this.httpClient.post('/api/upload', this.primaryPhoto, {responseType: 'json', params: {'sessionId': this.userService.sessionId}}).subscribe();
   }
 
-=======
->>>>>>> 99dc85e4248fc68005e6b603b3cdb843463a4ad6
   removePhotos() {
     this.primaryPhoto = null;
     this.patternPhoto = null;
@@ -81,16 +65,10 @@ export class UploaderComponent implements OnInit {
     this.newPatternPhoto.emit(this.patternPhoto);
   }
 
-<<<<<<< HEAD
   async processPhotos() {
     // do the stuff
     console.log('doing the stuff...' );
     this.httpClient.get('/api/evaluate')
-=======
-  processPhotos() {
-    // do the stuff
-    console.log('doing the stuff...' );
->>>>>>> 99dc85e4248fc68005e6b603b3cdb843463a4ad6
   }
 
 }
