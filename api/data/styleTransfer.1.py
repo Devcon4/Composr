c0577b1e36220882aad241b53b6b4380481a5a3b
Devcon4 <devcon42@gmail.com>
2019-07-15 07:10:23 -0600

    gen stuff!

---
+++ api/data/styleTransfer.1.py
@@ -0,0 +1,193 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import tensorflow as tf
+
+import matplotlib.pyplot as pyplot
+import matplotlib as mpl
+mpl.rcParams['figure.figsize'] = (12, 12)
+mpl.rcParams['axes.grid'] = False
+
+import numpy as np
+import time
+import functools
+import imageio
+import random
+
+session_hash = random.randrange(32*255)
+
+# https://www.tensorflow.org/beta/tutorials/generative/style_transfer
+content_path = tf.keras.utils.get_file('content_{}.jpg'.format(session_hash), 'http://localhost:5000/man.png')
+style_path = tf.keras.utils.get_file('style_{}.jpg'.format(session_hash),'http://localhost:5000/fireworks.png')
+# content_path = tf.keras.utils.get_file('turtle.jpg','https://storage.googleapis.com/download.tensorflow.org/example_images/Green_Sea_Turtle_grazing_seagrass.jpg')
+# style_path = tf.keras.utils.get_file('kandinsky.jpg','https://storage.googleapis.com/download.tensorflow.org/example_images/Vassily_Kandinsky%2C_1913_-_Composition_7.jpg')
+
+def load_img(path_to_img):
+    max_dim = 1024
+    img = tf.io.read_file(path_to_img)
+    img = tf.image.decode_image(img, channels=3)
+    img = tf.image.convert_image_dtype(img, tf.float32)
+
+    shape = tf.cast(tf.shape(img)[:-1], tf.float32)
+    long_dim = max(shape)
+    scale = max_dim / long_dim
+
+    new_shape = tf.cast(shape * scale, tf.int32)
+
+    img = tf.image.resize(img, new_shape)
+    img = img[tf.newaxis, :]
+    return img
+
+def imshow(image, title=None):
+    if len(image.shape) > 3:
+        image = tf.squeeze(image, axis=0)
+
+content_image = load_img(content_path)
+style_image = load_img(style_path)
+
+content_layers = ['block5_conv2']
+
+style_layers = [
+    'block1_conv1',
+    'block2_conv1',
+    'block3_conv1',
+    'block4_conv1',
+    'block5_conv1']
+
+num_content_layers = len(content_layers)
+num_style_layers = len(style_layers)
+
+def vgg_layers(layer_names):
+    vgg = tf.keras.applications.VGG19(include_top=False, weights='imagenet')
+    vgg.trainable = False
+
+    outputs = [vgg.get_layer(name).output for name in layer_names]
+
+    model = tf.keras.Model([vgg.input], outputs)
+    return model
+
+# There are several feature maps in the model graph, together they define the style of an image. a Gram Matrix is the best way to get this correlation
+def gram_matrix(input_tensor):
+    result = tf.linalg.einsum('bijc,bijd->bcd', input_tensor, input_tensor)
+    input_shape = tf.shape(input_tensor)
+    num_locations = tf.cast(input_shape[1] * input_shape[2], tf.float32)
+    return result/(num_locations)
+
+# Figure out the styles/content of an image.
+class StyleContentModel(tf.keras.models.Model):
+    def __init__(self, style_layers, content_layers):
+        super(StyleContentModel, self).__init__()
+        self.vgg = vgg_layers(style_layers + content_layers)
+        self.style_layers = style_layers
+        self.content_layers = content_layers
+        self.num_style_layers = len(style_layers)
+        self.vgg.trainable = False
+
+    def call(self, inputs):
+        inputs = inputs*255.0
+        preprocessed_input = tf.keras.applications.vgg19.preprocess_input(inputs)
+        outputs = self.vgg(preprocessed_input)
+        style_outputs, content_outputs = (outputs[:self.num_style_layers], outputs[self.num_style_layers:])
+
+        style_outputs = [gram_matrix(style_output) for style_output in style_outputs]
+
+        content_dict = {content_name:value for content_name, value in zip(self.content_layers, content_outputs)}
+
+        style_dict = {style_name:value for style_name, value in zip(self.style_layers, style_outputs)}
+
+        return {'content':content_dict, 'style':style_dict}
+
+# Implement the extracted style on the new image using gradient descent
+extractor = StyleContentModel(style_layers, content_layers)
+
+style_targets = extractor(style_image)['style']
+content_targets = extractor(content_image)['content']
+
+image = tf.Variable(content_image)
+
+def clip_0_1(image):
+    return tf.clip_by_value(image, clip_value_min=0.0, clip_value_max=1.0)
+
+opt = tf.optimizers.Adam(learning_rate=0.02, beta_1=0.99, epsilon=1e-1)
+
+style_weight=1e-2
+content_weight=1e4
+
+def style_content_loss(outputs):
+    style_outputs = outputs['style']
+    content_outputs = outputs['content']
+    style_loss = tf.add_n([tf.reduce_mean((style_outputs[name]-style_targets[name])**2) 
+                           for name in style_outputs.keys()])
+    style_loss *= style_weight / num_style_layers
+
+    content_loss = tf.add_n([tf.reduce_mean((content_outputs[name]-content_targets[name])**2) 
+                             for name in content_outputs.keys()])
+    content_loss *= content_weight / num_content_layers
+    loss = style_loss + content_loss
+    return loss
+
+def total_variation_loss(image):
+    x_deltas, y_deltas = high_pass_x_y(image)
+    return tf.reduce_mean(x_deltas**2) + tf.reduce_mean(y_deltas**2)
+
+total_variation_weight=1e8
+
+@tf.function()
+def train_step(image):
+  with tf.GradientTape() as tape:
+    outputs = extractor(image)
+    loss = style_content_loss(outputs)
+    loss += total_variation_weight*total_variation_loss(image)
+
+  grad = tape.gradient(loss, image)
+  opt.apply_gradients([(grad, image)])
+  image.assign(clip_0_1(image))
+
+def high_pass_x_y(image):
+    x_var = image[:,:,1:,:] - image[:,:,:-1,:]
+    y_var = image[:,1:,:,:] - image[:,:-1,:,:]
+
+    return x_var, y_var
+
+def saveImage(filename, image):
+  imageio.imwrite(filename, image)
+
+import time
+start = time.time()
+
+epochs = 20
+steps_per_epoch = 1000
+
+step = 0
+for n in range(epochs):
+  for m in range(steps_per_epoch):
+    step += 1
+    train_step(image)
+    print("#: {}, T+: {:.1f}".format(step, time.time()-start))
+  print("epoch: {}".format(n))
+  saveImage("run/epoch_{}.png".format(n), image[0])
+
+end = time.time()
+print("Total time: {:.1f}".format(end-start))
+
+saveImage("run/complete.png", image[0])
+# train_step(image)
+# train_step(image)
+# train_step(image)
+# train_step(image)
+# train_step(image)
+
+# imshow(image.read_value())
+
+
+
+# from scripy.misc import imsave
+# imsave("turtle.png", image[0])
+
+# mpl.image.imsave(file_name, image.read_value()[0], format='png')
+
+# try:
+#   from google.colab import files
+# except ImportError:
+#   pass
+# else:
+#   files.download(file_name)
